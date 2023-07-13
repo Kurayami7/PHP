eee447787fff89224dc39a284a3261bb6fb3096a
Areaf <darkgulliotine7@gmail.com>
2023-07-13 14:47:18 -0400

    Week 1

---
+++ week 1/page1.php
@@ -0,0 +1,24 @@
+<?php
+
+phpinfo();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First PHP page</title>
+</head>
+<body>
+    <h1>This is week one of PHP</h1>
+    <p>This language is odd, but that's to be expected when attempting to learn a new language</p>
+
+    <script>
+        console.log("This is a javascript console log statement");
+    </script>
+</body>
+</html>
+
