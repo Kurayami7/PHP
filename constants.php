8d95ca5e4a1bd6706fd61c7f4cca292df2788a14
Darkuser <124408792+Kurayami7@users.noreply.github.com>
2023-07-19 23:47:35 -0400

    Add files via upload

---
+++ constants.php
@@ -0,0 +1,9 @@
+<?php
+// Define function
+define('College', 'ABC Advanced Tech');
+
+echo "My college is College"; // Note that php treats it as a string literal
+
+echo "<br> My college is".College; // We must concatenate constants like so
+
+?>
