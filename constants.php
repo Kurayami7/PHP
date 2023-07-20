b66b4e32e8a55836185c44470cceaf56ce1375f8
Darkuser <124408792+Kurayami7@users.noreply.github.com>
2023-07-19 23:48:11 -0400

    Delete constants.php

--- constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Define function
-define('College', 'ABC Advanced Tech');
-
-echo "My college is College"; // Note that php treats it as a string literal
-
-echo "<br> My college is".College; // We must concatenate constants like so
-
-?>
